445eaa4ece596ddc313a409e694b3b8450cce1a1
Marley <marleyxiong0@gmail.com>
2020-12-03 22:35:53 -0700

    images work, data somewhat, yay

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "./styles/main.scss";
+import { data } from "./data";
+import images from "./img/*.*";
+const names = ["marley", "dmitri", "dhruvik"];
 const App = () => {
   return (
     <div>
       <h1>School 2.0 Yearbook</h1>
       <div className="grid-container">
-        <div id="marley" className="person">
-          <img src={require("./img/marley.jpeg")} alt="" />
-          <h3>Marley Smarley</h3>
-          <ul className="social-links">
-            <li>http://github.com/io0</li>
-            <li>http://marley.com</li>
-            <li>@marleymarlz</li>
-          </ul>
-          <p>
-            Nothing too complicated activity partners if you think we have
-            something in common easy-going. Long-term dating Netflix fixing my
-            scooter beach days trying this for the first time, glass half-full
-            what to order off of the menu no drama playing my guitar bored at
-            home.
-          </p>
-        </div>
+        {Object.entries(data).map(([name, info]) => {
+          console.log("./img/" + info.image);
+          return (
+            <div id={name} className="person">
+              <img src={Object.values(images[name])[0]} alt="" />{" "}
+              <h3>{info.name}</h3>
+              <ul className="social-links">
+                {info.github && <li>http://github.com/io0</li>}
+                {info.website && <li>http://marley.com</li>}
+                {info.twitter && <li>@marleymarlz</li>}
+              </ul>
+              <p>{info.description}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,117 @@
+export const data = {
+  athena: {
+    name: "Athena Leong",
+    image: "athena.jpeg",
+    website: "",
+    twitter: "",
+    description:
+      "Nothing too complicated activity partners if you think we have something in common easy-going. Long-term dating Netflix fixing my scooter beach days trying this for the first time, glass half-full what to order off of the menu no drama playing my guitar bored at home.",
+  },
+  azlen: {
+    name: "Azlen Elza",
+    image: "azlen.png",
+    website: "",
+    github: "",
+    twitter: "",
+    description: "",
+  },
+  chris: {
+    name: "Chris Axon",
+    image: "chris.jpeg",
+    website: "",
+    github: "",
+    twitter: "",
+    description: "",
+  },
+  dhruvik: {
+    name: "Dhruvik Parikh",
+    image: "dhruvik.jpeg",
+    github: "",
+    description: "",
+  },
+  dmitri: {
+    name: "Dmitri Brereton",
+    image: "dmitri.jpeg",
+    description: "",
+  },
+  ethan: {
+    name: "Ethan",
+    image: "ethan.jpeg",
+    description: "",
+  },
+  felipe: {
+    name: "Felipe",
+    image: "felipe.jpeg",
+    description: "",
+  },
+  harshu: {
+    name: "Harshu",
+    image: "harshu.jpeg",
+    description: "",
+  },
+  jonathan: {
+    name: "Jonathan",
+    image: "jonathan.jpeg",
+    description: "",
+  },
+  kesava: {
+    name: "Kesava",
+    image: "kesava.jpeg",
+    description: "",
+  },
+  liam: {
+    name: "Liam Hinzman",
+    image: "liam.jpeg",
+    description: "",
+  },
+  marley: {
+    name: "Marley",
+    image: "marley.jpeg",
+    description: "Marley smarley barley tarley darley",
+  },
+  matthew: {
+    name: "Matthew Siu",
+    image: "matthew.jpeg",
+    description: "",
+  },
+  noah: {
+    name: "Noah Trenaman",
+    image: "noah.jpg",
+    description: "",
+  },
+  raffi: {
+    name: "Raffi Hotter",
+    image: "raffi.jpeg",
+    description: "",
+  },
+  santi: {
+    name: "Santi Aranguri",
+    image: "santi.jpeg",
+    description: "",
+  },
+  steve: {
+    name: "Stephen Fay",
+    image: "steve.jpeg",
+    description: "",
+  },
+  taylor: {
+    name: "Taylor Rogalski",
+    image: "taylor.png",
+    description: "",
+  },
+  tiago: {
+    name: "Tiago Sada",
+    image: "tiago.png",
+    description: "",
+  },
+  vincent: {
+    name: "Vincent Huang",
+    image: "vincent.jpg",
+    description: "",
+  },
+  william: {
+    name: "William Bryk",
+    image: "william.png",
+    description: "",
+  },
+};
