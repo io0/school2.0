6eaa5e75930041dae4ed68d212b7a1a7177a5b1f
Vincent Huang <vincenthuang75025@yahoo.com>
2020-12-04 16:10:48 -0700

    india update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import images from "./img/*.*";
 const App = () => {
   const [activePerson, setActivePerson] = useState("");
   const [steves, setSteves] = useState([]);
+  const [overlayStyles, setOverlayStyles] = useState("");
 
   const onPersonHover = (name) => {
     setActivePerson(name);
@@ -16,9 +17,21 @@ const App = () => {
     setActivePerson("");
   };
 
+  const getImagePath = (name) => {
+    return Object.values(images[name])[0];
+  };
+
+  useEffect(() => {
+    if (activePerson === "noah") {
+      setOverlayStyles(`background-image: url(${getImagePath("india")})`);
+    } else {
+      setOverlayStyles("");
+    }
+  }, [activePerson]);
+
   useEffect(() => {
     let timeoutIDs = [];
-    if (activePerson == "steve" && !steves.length) {
+    if (activePerson === "steve" && !steves.length) {
       timeoutIDs = [...Array(10).keys()].map((n) => {
         window.setTimeout(() => {
           console.log(n);
@@ -43,93 +56,100 @@ const App = () => {
       `}
     >
       <div
+        id="overlay"
         css={css`
-          max-width: 1000px;
-          margin: 0 auto;
+          ${overlayStyles}
         `}
       >
-        <h1
+        <div
           css={css`
-            text-align: center;
-            font-size: 48px;
-            padding: 25px 0;
+            max-width: 1000px;
+            margin: 0 auto;
           `}
         >
-          {activePerson || `School 2.0 Yearbook`}
-        </h1>
-        <div className="grid-container">
-          {Object.entries(data).map(([name, info]) => {
-            // console.log("./img/" + info.image);
-            return (
-              <div
-                id={name}
-                className="person"
-                onMouseOver={() => onPersonHover(name)}
-                onMouseOut={onPersonHoverRemoved}
-              >
-                <img src={Object.values(images[name])[0]} alt="" />{" "}
-                <h2>{info.name}</h2>
-                <ul className="social-links">
-                  {info.github && (
-                    <li>
-                      <a href={info.github}>{info.github}</a>
-                    </li>
-                  )}
-                  {info.website && (
-                    <li>
-                      <a href={info.website}>{info.website}</a>
-                    </li>
-                  )}
-                  {info.twitter && (
-                    <li>
-                      <a href={`https://twitter.com/{info.twitter}`}>
-                        {info.twitter}
-                      </a>
-                    </li>
-                  )}
-                </ul>
-                <p>{info.description}</p>
-              </div>
-            );
-          })}
-          <div id="poggers" className="noHover">
-            POGGERS
-          </div>
-          <div id="dev" className="noHover">
-            9.2X developer
-          </div>
-          <div id="vincent-hey" className="noHover">
-            heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
-          </div>
-          <div id="india" className="noHover">
-            India.
-          </div>
-          <div class="box">
-            <b></b>
-            <b></b>
-            <b></b>
-            <b></b>
-            <b id="myboy"></b>
-          </div>
-          {steves.map((el) => (
-            <div
-              css={css`
-                position: fixed;
-                left: ${el.n * 100}px;
-                width: 100px;
-              `}
-              className="noHover"
-            >
-              <img
-                css={css`
-                  width: ${100 + el.n * 10}px;
-                `}
-                src={Object.values(images["steve"])[0]}
-                alt=""
-                className="noHover"
-              />
+          <h1
+            css={css`
+              text-align: center;
+              font-size: 48px;
+              padding: 25px 0;
+            `}
+          >
+            {activePerson || `School 2.0 Yearbook`}
+          </h1>
+          <div className="grid-container">
+            {Object.entries(data).map(([name, info]) => {
+              // console.log("./img/" + info.image);
+              return (
+                <div
+                  id={name}
+                  className="person"
+                  onMouseOver={() => onPersonHover(name)}
+                  onMouseOut={onPersonHoverRemoved}
+                >
+                  <img src={Object.values(images[name])[0]} alt="" />{" "}
+                  <h2>{info.name}</h2>
+                  <ul className="social-links">
+                    {info.github && (
+                      <li>
+                        <a href={info.github}>{info.github}</a>
+                      </li>
+                    )}
+                    {info.website && (
+                      <li>
+                        <a href={info.website}>{info.website}</a>
+                      </li>
+                    )}
+                    {info.twitter && (
+                      <li>
+                        <a href={`https://twitter.com/{info.twitter}`}>
+                          {info.twitter}
+                        </a>
+                      </li>
+                    )}
+                  </ul>
+                  <p>{info.description}</p>
+                </div>
+              );
+            })}
+            <div id="poggers" className="noHover">
+              POGGERS
             </div>
-          ))}
+            <div id="dev" className="noHover">
+              9.2X developer
+            </div>
+            <div id="vincent-hey" className="noHover">
+              heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+            </div>
+            <div id="india" className="noHover">
+              India.
+            </div>
+            <div class="box">
+              <b></b>
+              <b></b>
+              <b></b>
+              <b></b>
+              <b id="myboy"></b>
+            </div>
+            {steves.map((el) => (
+              <div
+                css={css`
+                  position: fixed;
+                  left: ${el.n * 100}px;
+                  width: 100px;
+                `}
+                className="noHover"
+              >
+                <img
+                  css={css`
+                    width: ${100 + el.n * 10}px;
+                  `}
+                  src={Object.values(images["steve"])[0]}
+                  alt=""
+                  className="noHover"
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
